afac00f59602aa3bce1c81f1a9d864e5cc297832
Kevin Sipahutar <69909883+vinss-droid@users.noreply.github.com>
2024-12-05 15:37:35 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,7 @@
 const tf = require('@tensorflow/tfjs-node');
-async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/asclepius-api/model.json');
+const modelURL= 'https://storage.googleapis.com/asclepius-api-dicoding/model.json'
+const envModelURL = process.env.MODEL_URL
+async function loadModel(envModelURL ?? modelURL) {
+    return tf.loadGraphModel();
 }
 module.exports = loadModel;
